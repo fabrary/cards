51d3b9f569afa1f1a472482e1571777035174abc
Phillip Manwaring <phillip.manwaring@gmail.com>
2023-03-12 13:14:06 -0400

    update tts

--- tts/index.ts
+++ tts/index.ts
@@ -2,17 +2,24 @@ import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { cards } from "../dist/index";
 
 const generateTTSCardNameAndDescriptions = () => {
-  return cards
+  const ttsCards = cards
     .flatMap((card) =>
       card.setIdentifiers.map((setIdentifier) => ({ setIdentifier, card }))
     )
     .sort((c1, c2) => c1.setIdentifier.localeCompare(c2.setIdentifier))
     .map(({ setIdentifier, card }) => {
       const pitch = card.pitch ? ` (${card.pitch})` : ``;
-      const text = card.functionalText?.replace(/\n/g, "\\n") || "";
-      return `["${setIdentifier}"] = { name = "${card.name}${pitch}", text = "'${text}'"}`;
+      const text =
+        card.functionalText?.replace(/\n/g, "\\n").replace(/"/g, '\\"') || "";
+      const isBack = card.isCardBack === true;
+      const keywords = card.keywords?.join(",") || "";
+      const cardTypes = card.types?.join(",") || "";
+      return `["${setIdentifier}${isBack ? "-BACK" : ""}"] = { name = "${
+        card.name
+      }${pitch}", text = "${text}", keywords = "${keywords}", types = "${cardTypes}"},`;
     })
-    .join("\n");
+    .join("\n  ");
+  return `OSCCardDB = {\n  ${ttsCards}\n}`;
 };
 
 const writeFiles = () => {
