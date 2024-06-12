cf4ab7277cc239820b6a58f8f5d6b877bde4ee1d
Phillip Manwaring <phillip.manwaring@gmail.com>
2024-06-12 17:53:55 -0400

    update tcgp for cosmo

--- packages/cards/tests/printings.test.ts
+++ packages/cards/tests/printings.test.ts
@@ -10,6 +10,10 @@ const riptide = cards.find(
   ({ cardIdentifier }) => cardIdentifier === "riptide-lurker-of-the-deep"
 );
 
+const cnc = cards.find(
+  ({ cardIdentifier }) => cardIdentifier === "command-and-conquer-red"
+);
+
 describe("Gets the correct default and special printings", () => {
   it("Riptide", () => {
     const card = riptide as Card;
@@ -22,5 +26,18 @@ describe("Gets the correct default and special printings", () => {
 
     expect(specialPrinting).toBeTruthy();
     expect(specialPrinting.identifier.includes("WIN")).toBeFalsy();
+  });
+
+  it("CnC", () => {
+    const card = cnc as Card;
+    const printings = card?.printings as Printing[];
+    const defaultPrinting = getDefaultPrinting(card, printings);
+    const specialPrinting = getSpecialPrinting(card, printings);
+
+    expect(defaultPrinting).toBeTruthy();
+    expect(defaultPrinting.identifier).toEqual("ARC159");
+
+    expect(specialPrinting).toBeTruthy();
+    expect(specialPrinting.identifier).toEqual("DYN000");
   });
 });
