f38499428a9da6c75f40e854e82ba2692a38da34
Phillip Manwaring <phillip.manwaring@gmail.com>
2024-07-27 17:52:38 -0400

    add notclassed to macros

--- packages/cards/scripts/Spoiled/mapper.ts
+++ packages/cards/scripts/Spoiled/mapper.ts
@@ -64,6 +64,9 @@ const getClasses = (card: ParsedCard): Class[] => {
   if (classes.length === 0 && getTalents(card)?.length) {
     classes.push(Class.NotClassed);
   }
+  if (classes.length === 0 && card.types.includes("Macro")) {
+    classes.push(Class.NotClassed);
+  }
   // if (classes.length === 0) {
   //   classes.push(Class.Generic);
   // }
