0635b8b28e16a9ad7cb071e630de40df89ae736a
Phillip Manwaring <phillip.manwaring@gmail.com>
2025-05-23 00:37:05 -0400

    add hero to set

--- packages/types/src/sets.ts
+++ packages/types/src/sets.ts
@@ -1394,7 +1394,7 @@ export const releases: ReleaseInfo[] = [
       // { url: "https://fabrary.net/decks/01JCPPENK52DTRBJZMWQF8S0X2" },
     ],
     draft: {
-      heroIdentifiers: ["gravy-bones", "marlynn", "puffin"],
+      heroIdentifiers: ["gravy-bones", "marlynn", "puffin", "scurv-stowaway"],
       picksPerPack: 14,
     },
     heroes: [Hero.GravyBones, Hero.Marlynn, Hero.Puffin],
