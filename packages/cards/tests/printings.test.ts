8048a787dc8997a766709def98f97885b6793be1
Phillip Manwaring <phillip.manwaring@gmail.com>
2026-01-11 15:20:37 -0500

    remove images not available yet

--- packages/cards/tests/printings.test.ts
+++ packages/cards/tests/printings.test.ts
@@ -35,7 +35,8 @@ describe("Gets the correct default and special printings", () => {
     const specialPrinting = getSpecialPrinting(card, printings);
 
     expect(defaultPrinting).toBeTruthy();
-    expect(defaultPrinting.identifier).toEqual("PEN319");
+    // expect(defaultPrinting.identifier).toEqual("PEN319");
+    expect(defaultPrinting.identifier).toEqual("HNT260");
 
     expect(specialPrinting).toBeTruthy();
     expect(specialPrinting.identifier).toEqual("DYN000");
